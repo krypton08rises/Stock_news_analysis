b949df3f52319c1763b2fdda971a2a462d55d24d
Kushagra Raina <kushagra.raina@gmail.com>
2024-04-30 11:34:57 +0100

    App version 0.4, forecasting using ARIMA

--- news_analysis_app.py
+++ news_analysis_app.py
@@ -1,4 +1,6 @@
+import datetime
 import gradio as gr
+import pandas as pd
 import yfinance as yf
 import seaborn as sns;
 
@@ -7,8 +9,11 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
 from datetime import date, timedelta
+from matplotlib import pyplot as plt
 from plotly.subplots import make_subplots
 from pytickersymbols import PyTickerSymbols
+from statsmodels.tsa.arima.model import ARIMA
+from pandas.plotting import autocorrelation_plot
 from dateutil.relativedelta import relativedelta
 
 index_options = ['FTSE 100(UK)', 'NASDAQ(USA)', 'CAC 40(FRANCE)']
@@ -17,9 +22,11 @@ ticker_dict = {'FTSE 100(UK)': 'FTSE 100', 'NASDAQ(USA)': 'NASDAQ 100', 'CAC 40(
 global START_DATE, END_DATE
 
 END_DATE = date.today()
-START_DATE = END_DATE - relativedelta(years=5)
+START_DATE = END_DATE - relativedelta(years=1)
+FORECAST_PERIOD = 7
 demo = gr.Blocks()
 stock_names = []
+
 with demo:
     d1 = gr.Dropdown(index_options, label='Please select Index...',
                      info='Will be adding more indices later on',
@@ -29,6 +36,29 @@ with demo:
 
 
     # d3 = gr.Dropdown(['General News'])
+
+    def forecast_series(series, model="ARIMA", forecast_period=7):
+
+        predictions = list()
+        if series.shape[1] > 1:
+            series = series['Close'].values.tolist()
+        plt.show()
+        if model == "ARIMA":
+            ## Do grid search here --> Custom for all stocks
+            for i in range(forecast_period):
+                model = ARIMA(series, order=(5, 1, 0))
+                model_fit = model.fit()
+                output = model_fit.forecast()
+                yhat = output[0]
+                predictions.append(yhat)
+                series.append(yhat)
+
+        return predictions
+
+
+    def is_business_day(a_date):
+        return a_date.weekday() < 5
+
 
     def get_stocks_from_index(idx):
         stock_data = PyTickerSymbols()
@@ -60,15 +90,30 @@ with demo:
                 ticker_name += 'L'
             else:
                 ticker_name += '.L'
+        elif ticker_dict[idx] == 'CAC 40':
+            ticker_name += '.PA'
 
         ## Can also download lower interval data apparently using line below
         # data = yf.download(tickers="MSFT", period="5d", interval="1m")
-        time_series = yf.download(tickers=ticker_name, start=START_DATE, end=END_DATE)  # stock.split(":")[1]
-        time_series.reset_index(inplace=True)
+        series = yf.download(tickers=ticker_name, start=START_DATE, end=END_DATE)  # stock.split(":")[1]
+        series = series.reset_index()
+
+        predictions = forecast_series(series)
+
+        last_date = pd.to_datetime(series['Date'].values[-1])
+        forecast_week = []
+
+        while len(forecast_week) != FORECAST_PERIOD:
+            if is_business_day(last_date):
+                forecast_week.append(last_date)
+            last_date += timedelta(days=1)
+
+        forecast = pd.DataFrame({"Date": forecast_week, "Forecast": predictions})
 
         fig = plt.figure(figsize=(14, 5))
         sns.set_style("ticks")
-        sns.lineplot(data=time_series, x="Date", y='Close', color='firebrick')
+        sns.lineplot(data=series, x="Date", y="Close", color="firebrick")
+        sns.lineplot(data=forecast, x="Date", y="Forecast", color="blue")
         sns.despine()
 
         plt.title("Stock Price of {}".format(stock_name), size='x-large', color='blue')  # stock.split(":")[0]
